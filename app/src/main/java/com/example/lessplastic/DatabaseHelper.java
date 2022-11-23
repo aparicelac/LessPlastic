6a84cfc2031015d45c8ac5c104edf380eb08fe1a
Alexandra Paricela Canazas <aparicelac@unsa.edu.pe>
2022-11-23 00:04:27 -0500

    Login y Main

---
+++ app/src/main/java/com/example/lessplastic/DatabaseHelper.java
@@ -0,0 +1,89 @@
+package com.example.lessplastic;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "lessPlastic.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String USUARIO_TABLE = "USUARIOS";
+    private static final String PLASTICOS_TABLE = "PLASTICOS";
+    private static final String REGISTROS_TABLE = "REGISTROS";
+    public static final String ID_USUARIO = "ID_usuario";
+    public static final String NOMBRE = "nombre";
+    public static final String NOMBRE_USUARIO = NOMBRE + "_usuario";
+    public static final String CORREO = "correo";
+    public static final String APELLIDO = "apellido";
+    public static final String CONTRASEÑA = "contraseña";
+    public static final String ID_PLASTICO = "ID_plastico";
+    public static final String TIPO = "tipo";
+    public static final String CANTIDAD = "cantidad";
+    public static final String TAMAÑO = "tamaño";
+    public static final String PESO = "peso";
+    public static final String FECHA = "fecha";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String usuarioTable = "CREATE TABLE " + USUARIO_TABLE + "(" + ID_USUARIO + " INTEGER PRIMARY KEY AUTOINCREMENT, " + NOMBRE_USUARIO + " TEXT, " + CORREO + " TEXT, " + NOMBRE + " TEXT, " + APELLIDO + " TEXT, " + CONTRASEÑA + " TEXT)";
+        db.execSQL(usuarioTable);
+        String plasticosTable = "CREATE TABLE " + PLASTICOS_TABLE + "(" + ID_PLASTICO + " INTEGER PRIMARY KEY AUTOINCREMENT, " + TIPO + " TEXT, " + CANTIDAD + " INTEGER, " + TAMAÑO + " REAL, " + PESO + " REAL)";
+        db.execSQL(plasticosTable);
+        String registrosTable = "CREATE TABLE " + REGISTROS_TABLE + "(" + FECHA + " TEXT, " + ID_USUARIO + " INTEGER, " + ID_PLASTICO + " INTEGER, FOREIGN KEY(" + ID_USUARIO + ") REFERENCES " + USUARIO_TABLE + "(" + ID_USUARIO + "), FOREIGN KEY(" + ID_PLASTICO + ") REFERENCES " + PLASTICOS_TABLE + "(" + ID_PLASTICO + "))";
+        db.execSQL(registrosTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    public boolean addUsuario(Usuario usuario) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        if (usuario != null) {
+            cv.put(NOMBRE_USUARIO, usuario.getNombreUsuario());
+            cv.put(CORREO, usuario.getCorreo());
+            cv.put(NOMBRE, usuario.getNombre());
+            cv.put(APELLIDO, usuario.getApellido());
+            cv.put(CONTRASEÑA, usuario.getContraseña());
+
+            long insert = db.insert(USUARIO_TABLE, null, cv);
+            if (insert == -1) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+        else {
+            return false;
+        }
+    }
+
+    public int checkUsuario(String usuario, String contraseña) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String check = "Select " + ID_USUARIO + ", " + NOMBRE_USUARIO + ", " + CONTRASEÑA + " from " + USUARIO_TABLE + " where " + NOMBRE_USUARIO + "= '" + usuario + "' and " + CONTRASEÑA + "='" + contraseña+ "'";
+        Cursor cursor = db.rawQuery(check, null);
+        if (cursor.getCount() > 0) {
+            cursor.moveToFirst();
+            int id = cursor.getInt(0);
+            return id;
+        }
+        else
+            return -1;
+
+    }
+}
---
+++ app/src/main/java/com/example/lessplastic/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.lessplastic;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.lessplastic.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        int ID_usuario = getIntent().getIntExtra("id_usuario", 0);
+        Toast.makeText(MainActivity.this, String.valueOf(ID_usuario), Toast.LENGTH_SHORT).show();
+
+        replaceFragment(new StatsFragment());
+        binding.bottomNavigationView.setBackground(null);
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.estadisticas:
+                    replaceFragment(new StatsFragment());
+                    break;
+
+                case R.id.calendario:
+                    replaceFragment(new CalendarFragment());
+                    break;
+
+                case R.id.logros:
+                    replaceFragment(new AwardsFragment());
+                    break;
+
+                case R.id.perfil:
+                    replaceFragment(new ProfileFragment());
+                    break;
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+}
